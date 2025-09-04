1ba5d25e790e80f6bc67ac7513fbfe750f32c39c
Alex Handley <smithvillescholarsah@gmail.com>
2025-09-04 14:08:37 -0500

    setting up

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const num1 = 6;
-const num2 = 8;
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-console.log(num1 + num2 === 14);
+app.disable("x-powered-by");
+app.use(express.static(path.join(__dirname)));
+
+app.get("/", (request, response) => {
+    response.sendFile(path.join(__dirname, "index.html"));
+})
+
+app.listen(PORT, () => {
+    console.log(`*** App is live on: localhost:${PORT} ***`);
+});
